273e2346738fa15ae4e18ae687496ca3b6ee9a52
Thomas Berthet <thoberth@student.42.fr>
2022-11-18 14:25:12 +0100

    add modif

--- Python01/ex05/the_bank.py
+++ Python01/ex05/the_bank.py
@@ -1,6 +1,7 @@
 # in the_bank.py
 class Account(object):
 	ID_COUNT = 1
+
 	def __init__(self, name, **kwargs):
 		self.__dict__.update(kwargs)
 		self.id = self.ID_COUNT
@@ -12,9 +13,40 @@ class Account(object):
 			raise AttributeError("Attribute value cannot be negative.")
 		if not isinstance(self.name, str):
 			raise AttributeError("Attribute name must be a str object.")
-			
+
 		def transfer(self, amount):
 			self.value += amount
 
-class Bank:
-	pass
+
+# in the_bank.py
+class Bank(object):
+	"""The bank"""
+
+	def __init__(self):
+	self.accounts = []
+
+	def add(self, new_account):
+	""" Add new_account in the Bank
+	@new_account: Account() new account to append
+	@return True if success, False if an error occured
+	"""
+	# test if new_account is an Account() instance and if
+	# it can be appended to the attribute accounts
+	# ... Your code ...
+	self.accounts.append(new_account)
+
+	def transfer(self, origin, dest, amount):
+	"""" Perform the fund transfer
+	@origin: str(name) of the first account
+	@dest: str(name) of the destination account
+	@amount: float(amount) amount to transfer
+	@return True if success, False if an error occured
+	"""
+	# ... Your code ...m
+
+	def fix_account(self, name):
+	""" fix account associated to name if corrupted
+	@name: str(name) of the account
+	@return True if success, False if an error occured
+	"""
+	# ... Your code ...
