1fb70b34dcab76d223e6c44af9ed18d523a95346
Thomas BERTHET <thoberth@students.42.fr>
2023-06-01 18:51:33 +0200

    add list comprehension

--- Python00/ex07/filterwords.py
+++ Python00/ex07/filterwords.py
@@ -1,19 +1,10 @@
 import sys
 
 if (__name__ == "__main__"):
-	if (sys.argv.__len__() != 3 or sys.argv[1].isnumeric() == True or sys.argv[2].isnumeric() != True):
+	if (len(sys.argv) != 3 or sys.argv[1].isnumeric() == True or sys.argv[2].isnumeric() != True):
 		print("ERROR")
 		exit(1)
-	list_to_print = []
-	word = ""
-	for i in range(sys.argv[1].__len__()):
-		if (sys.argv[1][i].isalpha() != True and word.__len__() > int(sys.argv[2])):
-			list_to_print.insert(list_to_print.__len__(), word)
-			word = ""
-		elif (sys.argv[1][i].isalpha() == True):
-			word += sys.argv[1][i]
-		else:
-			word = ""
-	if (word.__len__() > int(sys.argv[2])):
-		list_to_print.insert(list_to_print.__len__(), word)
+	word_len = int(sys.argv[2])
+	str_to_print = ''.join([c for c in sys.argv[1] if c.isalnum() == True or c == ' '])
+	list_to_print = [word for word in str_to_print.split(' ') if len(word) > word_len]
 	print(list_to_print)
